7999fe2e6cc84bdce380a801bd2e701e0aaa6df7
Geovana Moura <148665372+geofmoura@users.noreply.github.com>
2025-04-28 23:20:54 -0300

    Sair da aplicação

--- server/logout.php
+++ server/logout.php
@@ -1,13 +1,10 @@
 <?php
 session_start();
 
-// Limpa todas as variáveis de sessão
 $_SESSION = array();
 
-// Destroi a sessão
 session_destroy();
 
-// Redireciona para a página inicial
 header('Location: ../index.php');
 exit();
 ?>
